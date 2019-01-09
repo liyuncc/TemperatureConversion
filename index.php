37a364b448c2b594e97c3696108c8755b507d88d
TheBaldWonder <jonathan.d.heilman@seattlecentral.edu>
2019-01-08 18:06:58 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-
 <?php
-
     if(isset($_POST['inputValue']))
     {//data is submitted show it
        // echo $_POST['FirstName'];
@@ -11,6 +9,26 @@
         die;
         */
        $inputTemp=$_POST[inputValue]
+    switch $inputType:
+        case 'celsius':
+            $fahrenheit=$inputTemp*9/5+32;
+            $kelvin=$inputTemp+273.15;
+        echo $fahrenheit break; echo $kelvin break; 
+            break;
+        case 'fahrenheit':
+            $celsius=5/9*($inputTemp-32);
+            $kelvin=fahrenheit_to_celsius($inputTemp) + 273.15;
+        echo $celcius break; echo $kelvin break;
+            break;
+        case 'kelvin':
+            $fahrenheit=9/5*($inputTemp-273.15)+32;
+            $celsius=$inputTemp-273.15;
+        echo $fahrenheit break; echo celsius break;
+            break;
+    
+        default:
+            code to be executed if n is different from all labels;
+    }
         echo '<p><a href="">Do another calculation</a></p>';
         
     }else{//show a form
@@ -21,3 +39,5 @@
         ';
     }
 ?>
+
+
