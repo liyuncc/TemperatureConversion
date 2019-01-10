26b243e31ae5ca78545157cdb5a9621fe3df41a6
TheBaldWonder <jonathan.d.heilman@seattlecentral.edu>
2019-01-09 18:38:16 -0800

    Add files via upload
    Made updates for PSR-1 and PDR-2 standards

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php include 'includes/header.php'?>
-<?php        
-    if(isset($_POST["inputValue"]))
+<?php
+    if(isset($_POST['inputValue']))
     {//data is submitted show it
        // echo $_POST['FirstName'];
         /*
@@ -9,53 +8,36 @@
         echo '</pre>';
         die;
         */
-        $inputTemp=$_POST["inputValue"];
-        if (!is_numeric($inputTemp)) {//issue error response
-            echo '<p style="text-align:center;">Input value should be valid number.<BR />';  
-            echo '<a href="">Do another calculation</a></p>';
-        }else{
-            switch($_POST["inputType"]){
-                case 'celsius':
-                    $fahrenheit=$inputTemp*9/5+32;
-                    $kelvin=$inputTemp+273.15;
-                    echo '<p style="text-align:center;">' . $fahrenheit . '<BR />'; 
-                    echo $kelvin . '<BR />'; 
-                    echo '<a href="">Do another calculation</a></p>';
-                    break;
-                case 'fahrenheit':
-                    $celsius=5/9*($inputTemp-32);
-                    $kelvin=$inputTemp + 273.15;
-                    echo '<p style="text-align:center;">' . $celsius . '<BR />'; 
-                    echo $kelvin . '<BR />'; 
-                    echo '<a href="">Do another calculation</a></p>';
-                    break;
-                case 'kelvin':
-                    $fahrenheit=9/5*($inputTemp-273.15)+32;
-                    $celsius=$inputTemp-273.15;
-                    echo '<p style="text-align:center;">' . $fahrenheit . '<BR />'; 
-                    echo $celsius . '<BR />'; 
-                    echo '<a href="">Do another calculation</a></p>';
-                    break;    
-                default:
-                    echo '<p style="text-align:center;">Fatal Error</p>';
-                    die();
-            }
+       $inputTemp=$_POST[inputValue]
+    switch $inputType:
+        case 'celsius':
+            $fahrenheit=$inputTemp*9/5+32;
+            $kelvin=$inputTemp+273.15;
+        echo $fahrenheit break; echo $kelvin break; 
+            break;
+        case 'fahrenheit':
+            $celsius=5/9*($inputTemp-32);
+            $kelvin=fahrenheit_to_celsius($inputTemp) + 273.15;
+        echo $celcius break; echo $kelvin break;
+            break;
+        case 'kelvin':
+            $fahrenheit=9/5*($inputTemp-273.15)+32;
+            $celsius=$inputTemp-273.15;
+        echo $fahrenheit break; echo celsius break;
+            break;
+    
+        default:
+            code to be executed if n is different from all labels;
     }
-   }else{//show a form
+        echo '<p><a href="">Do another calculation</a></p>';
+        
+    }else{//show a form
         echo '
-                <form action="" method="post" name="TempCalc" id="tempCalcForm">                	
-                    <div style="text-align:center;">
-                        <div style="display:inline;"><input type="text" name="inputValue" placeholder="e.g. 32" size="12" style="height:22px;display:inline;"></div>
-                        <div style="display:inline;">
-                            <select name="inputType" style="height:22px;">
-                                <option value="fahrenheit">Fahrenheit</option>
-                                <option value="celsius">Celsius</option>
-                                <option value="kelvin">Kelvin</option>		
-                            </select>
-                        </div> 
-                        <div style="display:inline;"> <input type="submit" value="Convert"></div>
-                    </div>		 	                        
-                </form>';
+                  <form action="" method="post" name="TempCalc" id="tempCalcForm">
+                  <>
+                    </form>
+        ';
     }
 ?>
-<?php include 'includes/footer.php'?>
+
+
