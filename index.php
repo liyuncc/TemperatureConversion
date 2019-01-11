9e336c1e4ee8b0661809e876e07e5f579ebb914b
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-01-10 17:36:52 -0800

    Add entry prompt and results label

--- index.php
+++ index.php
@@ -21,22 +21,25 @@
                 case 'celsius':
                     $fahrenheit=$inputTemp*9/5+32;
                     $kelvin=$inputTemp+273.15;
+                    echo '<p style="text-align:center;">Here are your results:</p>'; 
                     echo '<p style="text-align:center;">' . number_format($fahrenheit,2). '&#176 Fahrenheit<BR />'; 
-                    echo number_format($kelvin,2) . '&#176 Kelvin<BR />'; 
+                    echo number_format($kelvin,2) . '&#176 Kelvin<BR /><BR />'; 
                     echo '<a href="">Do another calculation</a></p>';
                     break;
                 case 'fahrenheit':
                     $celsius=5/9*($inputTemp-32);
                     $kelvin=$inputTemp + 273.15;
+                    echo '<p style="text-align:center;">Here are your results:</p>'; 
                     echo '<p style="text-align:center;">' . number_format($celsius,2) . '&#176 Celsius<BR />'; 
-                    echo number_format($kelvin,2) . '&#176 Kelvin<BR />'; 
+                    echo number_format($kelvin,2) . '&#176 Kelvin<BR /><BR />'; 
                     echo '<a href="">Do another calculation</a></p>';
                     break;
                 case 'kelvin':
                     $fahrenheit=9/5*($inputTemp-273.15)+32;
                     $celsius=$inputTemp-273.15;
+                    echo '<p style="text-align:center;">Here are your results:</p>'; 
                     echo '<p style="text-align:center;">' . number_format($fahrenheit,2) . '&#176 Fahrenheit<BR />'; 
-                    echo number_format($celsius,2) . '&#176 Celsius<BR />'; 
+                    echo number_format($celsius,2) . '&#176 Celsius<BR /><BR />'; 
                     echo '<a href="">Do another calculation</a></p>';
                     break;    
                 default:
@@ -49,6 +52,7 @@
         echo '
                 <form action="index.php" method="post" name="TempCalc" id="tempCalcForm">                	
                     <div style="text-align:center;">
+                        <p>Input a temperature, select the type and click Convert</p>
                         <div style="display:inline;"><input type="text" name="inputValue" placeholder="e.g. 32" size="12" style="height:22px;display:inline;"></div>
                         <div style="display:inline;">
                             <select name="inputType" style="height:22px;">
